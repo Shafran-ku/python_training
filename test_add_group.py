5544aeb874d2126ac420e7828d80205f86ded9bf
Shafran-ku <shafran-ku@mail.ru>
2021-06-19 19:42:49 +0300

    Фикстура: тестовый класс разбили на 2 части - тесты остались в тестовом классе, все вспомогательные функции перенесены в отдельный класс Application(выступает в роли фикстуры)

--- test_add_group.py
+++ test_add_group.py
@@ -3,13 +3,13 @@ import pytest
 from group import Group
 from application import Application
 
-@pytest.fixture                                   #метка, превращает функцию в инициализатор фикстуры
-def app(request):                                 #функция создает фикстуру
+@pytest.fixture                                   # метка, превращает функцию в инициализатор фикстуры
+def app(request):                                 # функция создает фикстуру
     fixture = Application()
-    request.addfinalizer(fixture.destroy)         #для разрушения фикстуры
+    request.addfinalizer(fixture.destroy)         # для разрушения фикстуры
     return fixture
 
-def test_add_group(app):                            #тестовый метод, принимающий в качестве параметра фикстуру
+def test_add_group(app):                            # тестовый метод, принимающий в качестве параметра фикстуру
     app.login(username="admin", password="secret")
     app.create_group(Group(name="test5", header="xxx", footer="xxx"))
     app.logout()
